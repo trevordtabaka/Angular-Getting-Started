758114a935d5f81f492e7b0a631c8e8b925062d2
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2020-08-25 10:58:34 +0200

    add updates to code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { ProductListComponent } from './products/product-list.component';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
